19d6f3189340d1ba6f50f1895dd0a27d32d67715
erisgriffon <biochrome@gmail.com>
2017-09-19 13:53:03 -0700

    Add files via upload

--- opensesme.sh
+++ opensesme.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# OpenSESME v0.2.2
+# OpenSESME v0.3.0 - "Everything Is On Fire"
 # https://github.com/seattletimes/opensesme
-# E. A. Griffon - 2016-12-20
+# E. A. Griffon - 2017-03-20
 # Thanks to StackExchange, Yaro Kasear, Orville Broadbeak, and Skyler Bunny
-# http://unix.stackexchange.com/questions/24952/script-to-monitor-folder-for-new-files
 
 # Define where configurations are held
 CONFIG_DIR=/etc/opensesme.d/
 
 # Define where to log to
 LOGFILE=/var/log/opensesme.log
-
-# Empty the pid file (still in testing)
-echo > /tmp/opensesme.pid
 
 # Set our incrementalizers to 0
 # Error count
@@ -21,12 +17,6 @@ i=0
 r=0
 # Debug level
 d=0
-
-# Make a function for logging
-logit ()
-{
-:
-}
 
 # Make a function to be called to check config files
 # This needs to be fleshed out more to check for malformed paths, invalid characters, etc
@@ -37,7 +27,7 @@ configcheck ()
 		echo "Config $1 is missing ENABLED statement - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 is missing ENABLED statement - config invalid!"
 		((i++))
-	elif [ ! "$ENABLED" == "true" ] && [ ! "$ENABLED" == "false" ]; then
+	elif [[ ! "$ENABLED" == "true" ]] && [[ ! "$ENABLED" == "false" ]]; then
 		echo "Config $1 has ENABLED set to something other than 'true' or 'false' - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has ENABLED set to something other than 'true' or 'false' - config invalid!"
 		((i++))
@@ -53,41 +43,47 @@ configcheck ()
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has a malformed INPUT_DIR directory path (not absolute/does not start with /)"
 		((i++))
 	fi
+	
+	# Check for presence of Input Directory
+	if [[ ! -d "$INPUT_DIR" ]]; then
+		echo "Input Directory $INPUT_DIR doesn't seem to exist - config invalid!"
+		echo >> $LOGFILE "`date -Is` OpenSESME: Input Directory $INPUT_DIR doesn't seem to exist - config invalid!"
+		((i++))
+	fi
 
 	# Check config for Archive, Archive Directory, and Archive Filename statements
 	if ! grep --quiet "ARCHIVE\=" $1; then
 		echo "Config $1 is missing ARCHIVE statement - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 is missing ARCHIVE statement - config invalid!"
 		((i++))
-	elif [ ! "$ARCHIVE" == "true" ] && [ ! "$ARCHIVE" == "false" ]; then
+	elif [[ ! "$ARCHIVE" == "true" ]] && [[ ! "$ARCHIVE" == "false" ]]; then
 		echo "Config $1 has ARCHIVE set to something other than 'true' or 'false' - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has ARCHIVE set to something other than 'true' or 'false' - config invalid!"
 		((i++))
-	elif [ "$ARCHIVE" == "true" ] && ! grep --quiet "ARCHIVE_DIR\=" $1; then
+	elif [[ "$ARCHIVE" == "true" ]] && [[ ! "$RECURSIVE" == "true" ]] && ! grep --quiet "ARCHIVE_DIR\=" $1; then
 		echo "Config $1 has ARCHIVE set true but is missing ARCHIVE_DIR - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has ARCHIVE set true but is missing ARCHIVE_DIR - config invalid!"
 		((i++))
-	elif [ "$ARCHIVE" == "true" ] && ! grep --quiet "ARCHIVE_DIR\=\/" $1; then
+	elif [[ "$ARCHIVE" == "true" ]] && [[ ! "$RECURSIVE" == "true" ]] && ! grep --quiet "ARCHIVE_DIR\=\/" $1; then
 		echo "Config $1 has a malformed ARCHIVE_DIR directory path (not absolute/does not start with /)"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has a malformed ARCHIVE_DIR directory path (not absolute/does not start with /)"
 		((i++))
-	elif [ "$ARCHIVE" == "true" ] && ! grep --quiet "ARCHIVE_FILENAME\=" $1; then
+	elif [[ "$ARCHIVE" == "true" ]] && [[ ! "$RECURSIVE" == "true" ]] && ! grep --quiet "ARCHIVE_FILENAME\=" $1; then
 		echo "Config $1 has ARCHIVE set true but is missing ARCHIVE_FILENAME - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has ARCHIVE set true but is missing ARCHIVE_FILENAME - config invalid!"
 		((i++))
 	fi
-
 
 	# Check for Modify and Perform statements
 	if ! grep --quiet "MODIFY\=" $1; then
 		echo "Config $1 is missing MODIFY statement - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 is missing MODIFY statement - config invalid!"
 		((i++))
-	elif [ ! "$MODIFY" == "true" ] && [ ! "$MODIFY" == "false" ]; then
+	elif [[ ! "$MODIFY" == "true" ]] && [[ ! "$MODIFY" == "false" ]]; then
 		echo "Config $1 has MODIFY set to something other than 'true' or 'false' - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has MODIFY set to something other than 'true' or 'false' - config invalid!"
 		((i++))
-	elif [ "$MODIFY" == "true" ] && ! grep --quiet "PERFORM\=" $1; then
+	elif [[ "$MODIFY" == "true" ]] && ! grep --quiet "PERFORM\=" $1; then
 		echo "Config $1 has MODIFY set true, but is missing PERFORM statement - config invalid!"
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has MODIFY set true, but is missing PERFORM statement - config invalid!"
 		((i++))
@@ -111,7 +107,7 @@ configcheck ()
 		((i++))
 	fi
 
-	if [ ! $i -eq 0 ]; then
+	if [[ ! $i -eq 0 ]]; then
 		echo "The config has $i errors."
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has $i errors."
 		exit 1
@@ -119,11 +115,115 @@ configcheck ()
 		echo "Congratulations, the config has $i errors."
 		echo >> $LOGFILE "`date -Is` OpenSESME: Config $1 has $i errors."
 	fi
+
 }
 
+# A function used to archive a file with parameters from a sourced conf file
+# Called in the runconfig function
+archive ()
+{
+	# If a destination archive filename hasn't been set in the config, just use the given filename.
+	if [[ -z "$ARCHIVE_FILENAME" ]];
+		then ARCHIVE_FILENAME=$file
+	fi
+						
+	# Define NEWARCHIVENAME as the archive filename with timestamp, if requested
+	if [[ $ARCHIVE_DATESTAMP == "true" ]]
+		then
+			NEWARCHIVENAME=`date +%Y%m%d_%H%M%S`_$ARCHIVE_FILENAME
+		else	
+			NEWARCHIVENAME=$ARCHIVE_FILENAME
+	fi
+
+	cp $path/$file $ARCHIVE_DIR/$NEWARCHIVENAME && chown $OWNER.$GROUP $ARCHIVE_DIR/$NEWARCHIVENAME && chmod $PERMS $ARCHIVE_DIR/$NEWARCHIVENAME || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: archival copy has failed for $path/$file to $ARCHIVE_DIR/$NEWARCHIVENAME"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: archival copy has failed for $path/$file to $ARCHIVE_DIR/$NEWARCHIVENAME)
+
+	# Test to see if archive file is not actually there, and if it isn't, log errors
+	if [[ ! -e "$ARCHIVE_DIR/$NEWARCHIVENAME" ]]
+		then
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: File check has failed for presence of archived file in $ARCHIVE_DIR/$file"
+			logger -p local2.notice -t OpenSESME -- $ACTION_NAME: File check has failed for presence of archived file in $ARCHIVE_DIR/$NEWARCHIVENAME
+		else
+			# "Log" the events
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Archived $file from $path to $ARCHIVE_DIR/$NEWARCHIVENAME"
+	fi		
+}
+
+# For when inotifywait is used recursively ($RECURSIVE == true)
+# A function used to archive a file with parameters from a sourced conf file
+# Called in the runconfig function
+archive_recursive ()
+{
+	# Define NEWARCHIVENAME as the archive filename with timestamp, if requested
+	if [[ $ARCHIVE_DATESTAMP == true ]]
+		then
+			NEWARCHIVENAME=`date +%Y%m%d_%H%M%S`_$file
+		else	
+			NEWARCHIVENAME=$file
+	fi
+
+	cp $path/$file $path/$ARCHIVE_DIR/$NEWARCHIVENAME && chown $OWNER.$GROUP $path/$ARCHIVE_DIR/$NEWARCHIVENAME && chmod $PERMS $path/$ARCHIVE_DIR/$NEWARCHIVENAME || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: archival copy has failed for $path/$file to $path/$ARCHIVE_DIR/$NEWARCHIVENAME"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: archival copy has failed for $path/$file to $path/$ARCHIVE_DIR/$NEWARCHIVENAME)
+
+	# Test to see if archive file is not actually there, and if it isn't, log errors
+	if [[ ! -e "$path/$ARCHIVE_DIR/$NEWARCHIVENAME" ]]
+		then
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: File check has failed for presence of archived file in $path/$ARCHIVE_DIR/$file"
+			logger -p local2.notice -t OpenSESME -- $ACTION_NAME: File check has failed for presence of archived file in $path/$ARCHIVE_DIR/$NEWARCHIVENAME
+		else
+			# "Log" the events
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Archived $file from $path to $path/$ARCHIVE_DIR/$NEWARCHIVENAME"
+	fi		
+}
+
+# A function to modify a file with parameters passed from a sourced conf file
+# Called in the runconfig function
+modify ()
+{
+	# Define TEMPFILE as the filename with _tmp extension
+	TEMPFILE=$file\_tmp
+
+	# Move the file to /tmp for modification
+	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moving $path/$file to temporary directory for modification"
+	mv $path/$file /tmp/$TEMPFILE || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: mv has failed for $path/$file to /tmp/$TEMPFILE"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: mv has failed for $path/$file to /tmp/$TEMPFILE)
+
+	# Perform the modification
+	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Performing $PERFORM on /tmp/$TEMPFILE"
+	$PERFORM /tmp/$TEMPFILE ||(echo >> $LOGFILE "`date -Is` $ACTION_NAME: Execution of $PERFORM has failed on /tmp/$TEMPFILE"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: Execution of $PERFORM has failed on /tmp/$TEMPFILE)
+
+	# Move the modified file to the output directory
+	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moving /tmp/$TEMPFILE to $OUTPUT_DIR as $FILENAME"
+	mv /tmp/$TEMPFILE $OUTPUT_DIR/$FILENAME && chown $OWNER.$GROUP $OUTPUT_DIR/$FILENAME && chmod $PERMS $OUTPUT_DIR/$FILENAME || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: mv has failed for /tmp/$TEMPFILE to $OUTPUT_DIR/$FILENAME"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: mv has failed for /tmp/$TEMPFILE to $OUTPUT_DIR/$FILENAME)
+	}
+
+# A function to unset variables
+unset_vars ()
+{
+unset ENABLED
+unset ACTION_NAME
+unset MODIFY
+unset ARCHIVE
+unset ARCHIVE_DIR
+unset ARCHIVE_FILENAME
+unset ARCHIVE_DATESTAMP
+unset INPUT_DIR
+unset TARGET
+unset PERFORM
+unset OUTPUT_DIR
+unset FILENAME
+unset OWNER
+unset GROUP
+unset PERMS
+unset DATESTAMP
+unset RECURSIVE
+unset INO
+}
+	
 # Main function to run a given CONF file
 runconfig ()
 {
+
+	# First, unset any variables that may be lingering from a previous run/config file
+	unset_vars
+
 	echo -e "Reading $CONF"
 	
 	echo >> $LOGFILE "`date -Is` OpenSESME Main: Reading $CONF"
@@ -134,64 +234,83 @@ runconfig ()
 	configcheck $CONF
 
 	# Exit if the config is disabled
-	if [ $ENABLED == false ]
+	if [[ ! $ENABLED == "true" ]]
 	then
 		echo "`date -Is` OpenSESME Main: $CONF is disabled."
 		echo >> $LOGFILE "`date -Is` OpenSESME Main: $CONF is disabled."
 		exit 0
 	fi
-
+	
+	# Check for archive directories if config is set RECURSIVE=true and ARCHIVE=true
+	# Will create archive directories if they don't exist
+	if [[ $RECURSIVE == "true" ]] && [[ $ARCHIVE == "true" ]]; then
+		echo "Config is set to Recursive and Archive, checking for presence of archive directories and creating if missing."
+		for folder in $( ls $INPUT_DIR )
+			do 
+				if [[ ! -d $INPUT_DIR/$folder/archive ]]
+					then
+						mkdir $INPUT_DIR/$folder/archive
+				fi
+		done
+	fi
+	
 	# Log things about the action
 	logger -p local2.notice -t OpenSESME -- Starting action $ACTION_NAME
 	echo >> $LOGFILE "`date -Is` OpenSESME Main: Starting action $ACTION_NAME"
+	if [[ $RECURSIVE == "true" ]]
+		then
+			logger -p local2.notice -t OpenSESME -- $ACTION_NAME: Configured for recursive monitoring.
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Configured for recursive monitoring."
+	fi
 	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Input directory is $INPUT_DIR"
 	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Target is $TARGET"
-	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Archive directory is $ARCHIVE_DIR"
 	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Output directory is $OUTPUT_DIR"
 	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Archiving set to $ARCHIVE"
+	if [[ $ARCHIVE == "true" ]]
+		then
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Archive directory is $ARCHIVE_DIR"
+	fi
 	echo >> $LOGFILE "`date -Is` $ACTION_NAME: Modifications set to $MODIFY"
 
 	# inotify does the heavy lifting here - it'll monitor and let us know when a file is done being written to (i.e. a FTP transfer stops, move is done, etc)
-	inotifywait -m $INPUT_DIR -e close_write |
-
+	if [[ $RECURSIVE == "true" ]]; then
+			INO="inotifywait -m -r $INPUT_DIR -e close_write" 
+		else
+			INO="inotifywait -m $INPUT_DIR -e close_write" 
+	fi
+	
+	$INO |
+	
 	# Start a loop that pulls variables from inotifywait
 	while read path action file; do
-
+	
 		# Check to see if these are the droids we're looking for
-		if [[ $file != $TARGET ]]
-		then
+		if [[ $file != $TARGET ]]; then
 			continue
+		fi
+		
+		# If it's in an archive directory or the output directory, ignore it
+		if [[ $path == *"$ARCHIVE_DIR"* ]] || [[ $path == *"$OUTPUT_DIR"* ]]; then
+			continue
+		fi
+						
+		# If a destination filename hasn't been set in the config, just use the given filename.
+		if [[ -z "$FILENAME" ]];
+			then FILENAME=$file
 		fi
 
 		# Log that the file has appeared
 		echo >> $LOGFILE "`date -Is` $ACTION_NAME: The file $file appeared in directory $path via $action"
 
-		# Define NEWARCHIVENAME as the archive filename with timestamp, if requested
-		if [ $ARCHIVE_DATESTAMP == true ]
-		then
-			NEWARCHIVENAME=`date +%Y%m%d_%H%M%S`_$ARCHIVE_FILENAME
-		else
-			NEWARCHIVENAME=$ARCHIVE_FILENAME
-		fi
-		
 		# Archive an unmodified version
-		if [ $ARCHIVE == true ]
-		then
-			cp $path/$file $ARCHIVE_DIR/$NEWARCHIVENAME && chown $OWNER.$GROUP $ARCHIVE_DIR/$NEWARCHIVENAME && chmod $PERMS $ARCHIVE_DIR/$NEWARCHIVENAME || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: archival copy has failed for $path/$file to $ARCHIVE_DIR/$NEWARCHIVENAME"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: archival copy has failed for $path/$file to $ARCHIVE_DIR/$NEWARCHIVENAME)
-
-			# Test to see if archive file is not actually there, and if it isn't, log errors
-			if [ ! -e "$ARCHIVE_DIR/$NEWARCHIVENAME" ]
-			then
-				echo >> $LOGFILE "`date -Is` $ACTION_NAME: File check has failed for presence of archived file in $ARCHIVE_DIR/$file"
-				logger -p local2.notice -t OpenSESME -- $ACTION_NAME: File check has failed for presence of archived file in $ARCHIVE_DIR/$NEWARCHIVENAME
-			else
-				# "Log" the events
-				echo >> $LOGFILE "`date -Is` $ACTION_NAME: Archived $file from $path to $ARCHIVE_DIR/$NEWARCHIVENAME"
-			fi
+		if [[ $ARCHIVE == "true" ]] && [[ $RECURSIVE == "true" ]] && [[ ! $path == *"$ARCHIVE_DIR"* ]]; then
+			archive_recursive
+		elif  [[ $ARCHIVE == "true" ]] && [[ ! $RECURSIVE == "true" ]]; then
+			archive
 		fi
 
 		# Define NEWFILENAME as the filename with timestamp, if requested
-		if [ $DATESTAMP == true ]
+		if [[ $DATESTAMP == "true" ]]
 		then
 			NEWFILENAME=`date +%Y%m%d_%H%M%S`_$FILENAME
 		else
@@ -199,54 +318,53 @@ runconfig ()
 		fi
 
 		# Doin' work, movin the file
-		if [ $MODIFY == true ]
-		then
-			# Define TEMPFILE as the filename with _tmp extension
-			TEMPFILE=$file\_tmp
-			
-			# Move the file to /tmp for modification
-			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moving $path/$file to temporary directory for modification"
-			mv $path/$file /tmp/$TEMPFILE || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: mv has failed for $path/$file to /tmp/$TEMPFILE"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: mv has failed for $path/$file to /tmp/$TEMPFILE)
-			
-			# Perform the modification
-			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Performing $PERFORM on /tmp/$TEMPFILE"
-			$PERFORM /tmp/$TEMPFILE ||(echo >> $LOGFILE "`date -Is` $ACTION_NAME: Execution of $PERFORM has failed on /tmp/$TEMPFILE"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: Execution of $PERFORM has failed on /tmp/$TEMPFILE)
-			
-			# Move the modified file to the output directory
-			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moving /tmp/$TEMPFILE to $OUTPUT_DIR as $FILENAME"
-			mv /tmp/$TEMPFILE $OUTPUT_DIR/$FILENAME && chown $OWNER.$GROUP $OUTPUT_DIR/$FILENAME && chmod $PERMS $OUTPUT_DIR/$FILENAME || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: mv has failed for /tmp/$TEMPFILE to $OUTPUT_DIR/$FILENAME"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: mv has failed for /tmp/$TEMPFILE to $OUTPUT_DIR/$FILENAME)
-		
-		else
+		if [[ $MODIFY == "true" ]]; then
+			modify
+		elif [[ ! $RECURSIVE == "true" ]]; then
 			# If no modification, just move the file to the output directory with the new FILENAME
 			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moving $path/$file to $OUTPUT_DIR as $FILENAME"
 			mv $path/$file $OUTPUT_DIR/$FILENAME && chown $OWNER.$GROUP $OUTPUT_DIR/$FILENAME && chmod $PERMS $OUTPUT_DIR/$FILENAME || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: mv has failed for $path/$file to $OUTPUT_DIR/$FILENAME"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: mv has failed for $path/$file to $OUTPUT_DIR/$FILENAME)
+		elif [[ $RECURSIVE == "true" ]]; then
+			# If no modification, just move the file to the output directory with the original filename ($file)
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moving $path/$file to $OUTPUT_DIR as $file"
+			mv $path/$file $OUTPUT_DIR/$file && chown $OWNER.$GROUP $OUTPUT_DIR/$file && chmod $PERMS $OUTPUT_DIR/$file || (echo >> $LOGFILE "`date -Is` $ACTION_NAME: mv has failed for $path/$file to $OUTPUT_DIR/$file"; logger -p local2.notice -t OpenSESME -- $ACTION_NAME: mv has failed for $path/$file to $OUTPUT_DIR/$file)
 		fi
 
 		# Test to see if file is not actually there, and if it isn't, log errors
-		if
-			[ ! -e "$OUTPUT_DIR/$FILENAME" ]
-		then
-			echo >> $LOGFILE "`date -Is` $ACTION_NAME: File check has failed for presence of $OUTPUT_DIR/$FILENAME"
-			logger -p local2.notice -t OpenSESME -- $ACTION_NAME: File check has failed for presence of $OUTPUT_DIR/$FILENAME
-		else
-			# "Log" the events
+		if [[ ! $RECURSIVE == "true" ]] && [[ ! -e "$OUTPUT_DIR/$FILENAME" ]]; then
+				echo >> $LOGFILE "`date -Is` $ACTION_NAME: File check has failed for presence of $OUTPUT_DIR/$FILENAME"
+				logger -p local2.notice -t OpenSESME -- $ACTION_NAME: File check has failed for presence of $OUTPUT_DIR/$FILENAME
+		elif [[ ! $RECURSIVE == "true" ]] && [[ -e "$OUTPUT_DIR/$FILENAME" ]]; then
+			#Log Event
 			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moved $file from $path to $OUTPUT_DIR as $FILENAME"
+		elif [[ $RECURSIVE == "true" ]] && [[ ! -e "$OUTPUT_DIR/$file" ]]; then
+				echo >> $LOGFILE "`date -Is` $ACTION_NAME: File check has failed for presence of $OUTPUT_DIR/$file"
+				logger -p local2.notice -t OpenSESME -- $ACTION_NAME: File check has failed for presence of $OUTPUT_DIR/$file
+		elif [[ $RECURSIVE == "true" ]] && [[ -e "$OUTPUT_DIR/$file" ]]; then
+			#Log Event
+			echo >> $LOGFILE "`date -Is` $ACTION_NAME: Moved $file from $path to $OUTPUT_DIR as $file"
 		fi
 
+		if [[ $RECURSIVE == "true" ]]; then
+			FILENAME=""
+			NEWFILENAME=""
+		fi
+		
+		unset
+		
 	# Run the loop in the background with '&'
 	done &
-
-	# Record the pid of the last run program (not... quite working in a useful way)
-	#echo $! - $ACTION_NAME >>/tmp/opensesme.pid
-
+		
 	# If this was called with an option, exit, otherwise continue to parse config files
-	if [ ! $r -eq 0 ]; then
+	if [[ ! $r -eq 0 ]]; then
 		exit 0
 	fi
 }
 
-# Check for flags here (under construction!)
-while getopts ":f:c:d:" opt; do
+
+
+# Check for flags here
+while getopts ":f:c:" opt; do
 	case $opt in
 		# Run one specified config file
 		f)
@@ -263,11 +381,6 @@ while getopts ":f:c:d:" opt; do
 			configcheck $OPTARG
 			((r++))
 			exit 0
-		;;
-		# Set debug level
-		d)
-			echo Setting debug level to $OPTARG
-			#LOGLEVEL=$OPTARG
 		;;
 		# Invalid switch handling
 		\?)
